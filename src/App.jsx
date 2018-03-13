edb3a06d5ee81aebef656c0d0b986f86beacfb51
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-03-13 14:52:03 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div id="app">
+        <h1>React is working fine!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App.jsx';
+
+var dest = document.getElementById('root');
+
+ReactDOM.render(
+  <App />,
+  dest
+);
